bba81d09715b4ba091ae73ce546d3b371215290f
Lydia Greco <lydiagreco@Lydias-MacBook-Air.local>
2018-04-04 12:30:33 -0600

    Updated to current

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,5 +1,5 @@
 class Identity < ApplicationRecord
   validates_presence_of :name
   validates_uniqueness_of :email
-  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+  validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 end

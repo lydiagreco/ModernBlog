61b11ceab1b0e227eabd7da2fe85c74777a3c97f
Lydia Greco <lydiagreco@Lydias-MacBook-Air.local>
2018-04-11 14:00:17 -0600

    config searchbox

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,7 +2,17 @@ require 'elasticsearch/model'
 
 class Blog < ApplicationRecord 
   include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
+  after_commit on: [:create] do
+   __elasticsearch__.index_document
+  end
+
+  after_commit on: [:update] do
+   __elasticsearch__.index_document
+  end
+
+  after_commit on: [:destroy] do
+   __elasticsearch__.delete_document
+  end 
   enum status: { draft: 0, published: 1 }
   validates_presence_of :title, :body
   settings do

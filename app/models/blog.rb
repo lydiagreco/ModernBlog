8a82a44cdb2d05157a048da3fd89fcd745fd4e4c
Lydia Greco <lydiagreco@Lydias-MacBook-Air.local>
2018-04-12 15:17:04 -0600

    Changing callback for elasticsearch

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,7 +2,38 @@ require 'elasticsearch/model'
 
 class Blog < ApplicationRecord 
   include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
+  after_commit on: [:create] do
+    begin
+      __elasticsearch__.index_document
+    rescue Exception => ex
+      logger.error "ElasticSearch after_commit error on create: #{ex.message}"
+    end
+  end
+
+  after_commit on: [:update] do
+    begin
+      Elasticsearch::Model.client.exists?(index: 'articles', type: 'article', id: self.id) ? __elasticsearch__.update_document :     __elasticsearch__.index_document
+    rescue Exception => ex
+      logger.error "ElasticSearch after_commit error on update: #{ex.message}"
+    end
+  end
+
+  after_commit on: [:destroy] do
+    begin
+      __elasticsearch__.delete_document
+    rescue Exception => ex
+      logger.error "ElasticSearch after_commit error on delete: #{ex.message}"
+    end
+  end
+
+  def as_indexed_json(options={})
+    as_json({
+      only: [ :id, :article_number, :user_id, :article_type, :comments, :posts, :replies, :status, :fb_share, :google_share, :author, :contributor_id, :created_at, :updated_at ],
+      include: {
+        posts: { only: [ :id, :article_id, :post ] },
+      }
+    })
+  end
   enum status: { draft: 0, published: 1 }
   validates_presence_of :title, :body
   settings do

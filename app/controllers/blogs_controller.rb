8ba30a77e55b8d66718260cd7d5598b68e91f098
Lydia Greco <lydiagreco@Lydias-MacBook-Air.local>
2018-05-09 14:31:14 -0600

    Elasticsearch is running and functional

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -82,14 +82,14 @@ class BlogsController < ApplicationController
    query = params[:search_blogs].presence && params[:search_blogs][:query]
 
    if query
-     @blogs = Blog.search_published(query)
+     @blogs = Blog.search(query)
    end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params[:id])
+      @blog = Blog.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,71 +2,72 @@ require 'elasticsearch/model'
 
 class Blog < ApplicationRecord 
   include Elasticsearch::Model
-  after_commit on: [:create] do
-    begin
-      __elasticsearch__.index_document
-    rescue Exception => ex
-      logger.error "ElasticSearch after_commit error on create: #{ex.message}"
-    end
-  end
+  include Elasticsearch::Model::Callbacks
+  
+  index_name Rails.application.class.parent_name.underscore
+  document_type self.name.downcase
 
-  after_commit on: [:update] do
-    begin
-      Elasticsearch::Model.client.exists?(index: 'articles', type: 'article', id: self.id) ? __elasticsearch__.update_document :     __elasticsearch__.index_document
-    rescue Exception => ex
-      logger.error "ElasticSearch after_commit error on update: #{ex.message}"
-    end
-  end
-
-  after_commit on: [:destroy] do
-    begin
-      __elasticsearch__.delete_document
-    rescue Exception => ex
-      logger.error "ElasticSearch after_commit error on delete: #{ex.message}"
-    end
-  end
-
-  enum status: { draft: 0, published: 1 }
-  validates_presence_of :title, :body
-  settings do |blog|
-    mappings dynamic: false do
-      indexes :author, type: :text
-      indexes :title, type: :text, analyzer: :english
-      indexes :body, type: :text, analyzer: :english
-      indexes :tags, type: :text, analyzer: :english
+  settings index: { number_of_shards: 1 } do
+    mapping dynamic: false do
+      indexes :title, analyzer: 'english'
+      indexes :body, analyzer: 'english'
       indexes :published, type: :boolean
     end
   end
+  
+  enum status: { draft: 0, published: 1 }
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+  validates_presence_of :title, :body
+  
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ['title^5', 'body']
+          }
+        },
+        highlight: {
+          pre_tags: ['<mark>'],
+          post_tags: ['</mark>'],
+          fields: {
+            title: {},
+            body: {},
+          }
+        },
+        suggest: {
+          text: query,
+          title: {
+            term: {
+              size: 1,
+              field: :title
+            }
+          },
+          body: {
+            term: {
+              size: 1,
+              field: :body
+            }
+          }
+        }
+      }
+    )
+  end
 
-  def self.search_published(query)
-   self.search({
-     query: {
-       bool: {
-         must: [
-         {
-           multi_match: {
-             query: query,
-             fields: [:author, :title, :body, :tags]
-           }
-         },
-         {
-           match: {
-             published: true
-           }
-         }]
-       }
-     }
-   })
- end
+  def as_indexed_json(options = nil)
+    self.as_json( only: [ :title, :body ] )
+  end
+
+    # Delete the previous articles index in Elasticsearch
+  Blog.__elasticsearch__.client.indices.delete index: Blog.index_name rescue nil
+
+  # Create the new index with the new mapping
+  Blog.__elasticsearch__.client.indices.create \
+    index: Blog.index_name,
+    body: { settings: Blog.settings.to_hash, mappings: Blog.mappings.to_hash }
+
+  # Index all article records from the DB to Elasticsearch
+  Blog.import
 end
-
-# Delete the previous articles index in Elasticsearch
-Blog.__elasticsearch__.client.indices.delete index: Blog.index_name rescue nil
-
-# Create the new index with the new mapping
-Blog.__elasticsearch__.client.indices.create \
-  index: Blog.index_name,
-  body: { settings: Blog.settings.to_hash, mappings: Blog.mappings.to_hash }
-
-# Index all article records from the DB to Elasticsearch
-Blog.import

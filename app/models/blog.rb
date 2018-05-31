febce31dd1498ee72784327c4d5c890300902e16
Lydia Greco <lydiagreco@Lydias-MacBook-Air.local>
2018-05-30 23:32:10 -0600

    undo previous callback config to test faraday in prod

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,30 +2,7 @@ require 'elasticsearch/model'
 
 class Blog < ApplicationRecord 
   include Elasticsearch::Model
-  after_commit on: [:create] do
-    begin
-      __elasticsearch__.index_document
-    rescue Exception => ex
-      logger.error "ElasticSearch after_commit error on create: #{ex.message}"
-    end
-  end
-
-  after_commit on: [:update] do
-    begin
-      Elasticsearch::Model.client.exists?(index: 'articles', type: 'article', id: self.id) ? __elasticsearch__.update_document :     __elasticsearch__.index_document
-    rescue Exception => ex
-      logger.error "ElasticSearch after_commit error on update: #{ex.message}"
-    end
-  end
-
-  after_commit on: [:destroy] do
-    begin
-      __elasticsearch__.delete_document
-    rescue Exception => ex
-      logger.error "ElasticSearch after_commit error on delete: #{ex.message}"
-    end
-  end
-
+  include Elasticsearch::Model::Callbacks
   
   index_name Rails.application.class.parent_name.underscore
   document_type self.name.downcase
